8f210f1d41b2babca8a25d1e10aad524dce02f97
Axel von Engel <a.vonengel@gmail.com>
2018-12-26 08:23:23 +0100

    First version that parses the ID

--- adoc_extension/src/main/java/com/github/avonengel/adocspec/SpecTreeProcessor.java
+++ adoc_extension/src/main/java/com/github/avonengel/adocspec/SpecTreeProcessor.java
@@ -4,6 +4,7 @@ import org.asciidoctor.ast.Document;
 import org.asciidoctor.ast.StructuralNode;
 import org.asciidoctor.extension.Treeprocessor;
 import org.itsallcode.openfasttrace.core.SpecificationItem;
+import org.itsallcode.openfasttrace.core.SpecificationItemId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,15 +21,15 @@ public class SpecTreeProcessor extends Treeprocessor {
 
     @Override
     public Document process(Document document) {
-        LOG.info("Examining document: {}", document.getDoctitle());
-        searchBlocksForSpecSections(document);
-        Map<String, Object> config = getConfig();
+        LOG.debug("Examining document: {}", document.getDoctitle());
+        searchDocumentForSpecSections(document);
         return document;
     }
 
-    private void searchBlocksForSpecSections(Document document) {
+    private void searchDocumentForSpecSections(Document document) {
         if (document != null) {
             Map<Object, Object> findBy = new HashMap<>();
+            findBy.put("role", "spec");
             List<StructuralNode> specNodes = document.findBy(findBy);
             specNodes.forEach(this::processSpecNode);
         }
@@ -36,7 +37,19 @@ public class SpecTreeProcessor extends Treeprocessor {
 
     private void processSpecNode(StructuralNode structuralNode) {
         SpecificationItem.Builder specBuilder = SpecificationItem.builder();
+        Map<String, Object> attributes = structuralNode.getAttributes();
         specBuilder.id("test", "test", 1);
+        LOG.debug("node ID: {}", structuralNode.getId());
+        LOG.debug("node attributes: {}", structuralNode.getAttributes());
+        Object specID = structuralNode.getAttribute("specID");
+        LOG.debug("specID attribute: {}", specID);
+        LOG.debug("node title: {}", structuralNode.getTitle());
+        LOG.debug("node context: {}", structuralNode.getContext());
+        Object specIdAttributeValue = structuralNode.getAttribute("specID");
+        if(specIdAttributeValue instanceof String) {
+            specBuilder.id(SpecificationItemId.parseId((String) specIdAttributeValue));
+        }
+
         specificationItems.add(specBuilder.build());
     }
 
--- adoc_extension/src/test/java/SpecTreeProcessorTest.java
+++ adoc_extension/src/test/java/SpecTreeProcessorTest.java
@@ -1,39 +1,78 @@
 import com.github.avonengel.adocspec.SpecTreeProcessor;
 import org.asciidoctor.Asciidoctor;
 
+import static java.util.stream.Collectors.toList;
 import static org.asciidoctor.OptionsBuilder.options;
 
 import org.hamcrest.Matchers;
 import org.itsallcode.openfasttrace.core.SpecificationItem;
+import org.itsallcode.openfasttrace.core.SpecificationItemId;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.asciidoctor.Asciidoctor.Factory.create;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
 
+@DisplayName("A SpecObject")
 public class SpecTreeProcessorTest {
 
+    private static final Logger LOG = LoggerFactory.getLogger(SpecTreeProcessorTest.class);
+
     @Test
-    public void test() {
+    @DisplayName("is defined by using the .spec role")
+    public void whenSpecRoleIsFoundOnSectionSpecItemIsCreated() {
         // Arrange
         String input = "[.spec]\n" +
-                "## A specification section";
+                "== A specification section";
         Asciidoctor asciidoctor = create();
         asciidoctor.unregisterAllExtensions();
         SpecTreeProcessor underTest = new SpecTreeProcessor();
         asciidoctor.javaExtensionRegistry().treeprocessor(underTest);
 
         // Act
-        String output = asciidoctor.convert("", options().toFile(false));
+        String output = asciidoctor.convert(input, options().toFile(false));
+        LOG.info("Output: {}", output);
 
         // Assert
         List<SpecificationItem> specObjects = underTest.getSpecObjects();
         assertThat(specObjects, notNullValue());
         assertThat(specObjects, not(empty()));
+    }
+
+    @DisplayName("has a SpecificationItemId")
+    @Nested
+    class SpecificationItemIdTest {
+        @Test
+        @DisplayName("that can be defined in OFT syntax via specID attribute")
+        void specItemIdCanBeDefinedInOftSyntax() {
+            // Arrange
+            String input = "[.spec,specID=feat~html-export~1]\n" +
+                    "== HTML Export";
+            Asciidoctor asciidoctor = create();
+            asciidoctor.unregisterAllExtensions();
+            SpecTreeProcessor underTest = new SpecTreeProcessor();
+            asciidoctor.javaExtensionRegistry().treeprocessor(underTest);
+
+            // Act
+            String output = asciidoctor.convert(input, options().toFile(false));
+
+            // Assert
+            List<SpecificationItem> specObjects = underTest.getSpecObjects();
+            assertThat(specObjects.stream()
+                            .map(SpecificationItem::getId)
+                            .collect(toList()),
+                    hasItem(SpecificationItemId.parseId("feat~html-export~1")));
+        }
     }
 }
